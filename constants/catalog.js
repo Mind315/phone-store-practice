16359fd4f9666b08be648eee073e822a03cd9cfa
Oleg Mind <oleg22597@mail.ru>
2022-05-15 11:05:12 +0300

    push project

---
+++ constants/catalog.js
@@ -0,0 +1,62 @@
+const CATALOG = [
+    {
+        id: "el1",
+        name: "Honor 50 6/128GB (изумрудно-зеленый)",
+        img: "https://i.ibb.co/dG77W8K/download.jpg",
+        price: 34990,
+    },
+    {
+        id: "el2",
+        name: "Apple iPhone 13 Pro Max 256GB (небесно-голубой)",
+        img: "https://i.ibb.co/zF4pZ9y/download-1.jpg",
+        price: 179000,
+    },
+    {
+        id: "el3",
+        name: "Samsung Galaxy S21+ 5G 8/128GB (черный фантом)",
+        img: "https://i.ibb.co/fqMF78k/download-1.jpg",
+        price: 89900,
+    },
+    {
+        id: "el4",
+        name: "Samsung Galaxy A32 4/128GB (лаванда) ",
+        img: "https://i.ibb.co/cXkw6y9/download-1.jpg",
+        price: 27900,
+    },
+    {
+        id: "el5",
+        name: "Apple iPhone 12 256GB (фиолетовый)",
+        img: "https://i.ibb.co/NxsvbYj/download-1.jpg",
+        price: 79990,
+    },
+    {
+        id: "el6",
+        name: "Samsung Galaxy A52 4/128GB (черный)",
+        img: "https://i.ibb.co/hY5CDMY/download-1.jpg",
+        price: 32990,
+    },
+    {
+        id: "el7",
+        name: "Xiaomi Redmi Note 10S 6/128GB (синий океан)",
+        img: "https://i.ibb.co/Z6jJsD0/download-1.jpg",
+        price: 21990,
+    },
+    {
+        id: "el8",
+        name: "Apple iPhone 11 64GB (белый)",
+        img: "https://i.ibb.co/6YDvpd6/download-1.jpg",
+        price: 49990,
+    },
+    {
+        id: "el9",
+        name: "Apple iPhone 13 Pro 256GB (графитовый)",
+        img: "https://i.ibb.co/DpSJQXn/download-1.jpg",
+        price: 139990,
+    },
+    {
+        id: "el10",
+        name: "Honor X8 6/128GB (серебристый)",
+        img: "https://i.ibb.co/PCJbbfv/download-1.jpg",
+        price: 27990,
+    },
+];
